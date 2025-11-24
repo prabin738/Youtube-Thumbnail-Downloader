bf0db7248392244e870ae7685b2ffe8cfb3e4ff8
prabin738 <prabinsingh750@gmail.com>
2025-11-24 16:38:14 +0545

    app done completly

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,107 @@
 import "remixicon/fonts/remixicon.css";
 import "animate.css";
 import "./App.css";
+import { useState } from "react";
+import getYouTubeID from "get-youtube-id";
+import { ToastContainer, toast } from "react-toastify";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const urlModel = [
+    {
+      width: 120,
+      height: 90,
+      url: "https://img.youtube.com/vi",
+      filename: "default.jpg",
+    },
+    {
+      width: 320,
+      height: 180,
+      url: "https://img.youtube.com/vi",
+      filename: "mqdefault.jpg",
+    },
+    {
+      width: 480,
+      height: 360,
+      url: "https://img.youtube.com/vi",
+      filename: "hqdefault.jpg",
+    },
+    {
+      width: 640,
+      height: 480,
+      url: "https://img.youtube.com/vi",
+      filename: "stdefault.jpg",
+    },
+    {
+      width: 1280,
+      height: 720,
+      url: "https://img.youtube.com/vi",
+      filename: "maxresdefault.jpg",
+    },
+  ];
+
+  const [url, setUrl] = useState("");
+  const [thumbnails, setThumbnails] = useState([]);
+
+  const fetchThumbnail = (e) => {
+    e.preventDefault();
+    const videoId = getYouTubeID(url);
+    console.log(videoId);
+    if (videoId) {
+      const model = urlModel.map((item) => {
+        return {
+          ...item,
+          url: `${item.url}/${videoId}/${item.filename}`,
+        };
+      });
+      setThumbnails(model);
+      // console.log(model);
+    } else {
+      toast.error("Invalid video url");
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="min-h-screen bg-gray-200 py-8 ">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold">Youtube Thumbnail Downloader</h1>
+        <form className="space-x-4 mt-8" onSubmit={fetchThumbnail}>
+          <input
+            type="url"
+            className="bg-white p-3 rounded-lg w-[450px]"
+            required
+            placeholder="Enter Youtube Video URL Here "
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <button className="p-3 cursor-pointer bg-indigo-600 text-white rounded-lg font-medium">
+            <i className="ri-search-line mr-1"></i>
+            Search
+          </button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="grid grid-cols-3 gap-12 w-10/12 mx-auto mt-8">
+        {thumbnails.map((item, index) => (
+          <div className="bg-white rounded-lg " key={index}>
+            <img
+              src={item.url}
+              className="w-full h-[250px] object-cover rounded-t-xl"
+            />
+            <div className="p-3 bg-white rounded-b-xl">
+              <h1 className="text-xl font-medium">
+                {item.width}x{item.height}
+              </h1>
+              <a href={item.url} target="blank">
+                <button className=" mt-3 px-4 py-2 cursor-pointer  bg-green-500 text-white rounded-lg font-medium">
+                  <i className=" ri-download-line mr-1"></i>
+                  Download
+                </button>
+              </a>
+            </div>
+          </div>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <ToastContainer />
+    </div>
   );
 }
 
